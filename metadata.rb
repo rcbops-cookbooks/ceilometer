08bc8514a11aa4445e2cf491b9bb39f1d54abe07
rackerjoe <breu@breu.org>
2013-09-09 16:16:19 -0500

    Updating version # to 4.1.2

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer       "Rackspace US, Inc."
 license          "Apache 2.0"
 description      "Installs/Configures ceilometer"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.1"
+version          "4.1.2"
 
 %w{ ubuntu }.each do |os|
   supports os
