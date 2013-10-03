0b991a65d9065220805bbf1df2fd3033c56cb9fb
cloudnull <kevin.carter@rackspace.com>
2013-10-03 14:24:33 -0500

    added ha queues and logging

--- recipes/ceilometer-common.rb
+++ recipes/ceilometer-common.rb
@@ -29,8 +29,10 @@ glance_notification_topic =
   get_settings_by_role("glance-setup", "glance")["api"]["notification_topic"]
 
 mysql_connect_ip = get_access_endpoint("mysql-master", "mysql", "db")["host"]
-#TODO(mancdaz) - search for a list of rabbits and use HA rabbit queues
+
+# Get my Rabbit Queues and Settings
 rabbit_info = get_access_endpoint("rabbitmq-server", "rabbitmq", "queue")
+rabbit_settings = get_settings_by_role("rabbitmq-server", "rabbitmq")
 
 platform_options["supporting_packages"].each do |pkg|
   package pkg do
@@ -62,6 +64,8 @@ template "/etc/ceilometer/ceilometer.conf" do
   group "ceilometer"
   mode "0660"
   variables(
+    "verbose" => ceilometer["logging"]["verbose"],
+    "debug" => ceilometer["logging"]["debug"],
     "metering_secret" => ceilometer["metering_secret"],
     "mysql_user" => ceilometer["db"]["username"],
     "mysql_password" => ceilometer["db"]["password"],
@@ -79,6 +83,7 @@ template "/etc/ceilometer/ceilometer.conf" do
     "keystone_auth_host" => ks_admin_endpoint["host"],
     "rabbit_host" => rabbit_info["host"],
     "rabbit_port" => rabbit_info["port"],
+    "rabbit_ha_queues" => rabbit_settings["cluster"] ? "True" : "False",
     "glance_notification_topic" => glance_notification_topic
   )
 end
